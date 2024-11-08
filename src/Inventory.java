bc49c16ea4dcf138968e6dbaeb41bab811724cf4
varshankumar <varshankumar05@gmail.com>
2024-11-07 19:08:01 -0800

    initial

--- src/Inventory.java
+++ src/Inventory.java
@@ -4,8 +4,11 @@ import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.awt.Frame;
 import java.awt.TextArea;
-import org.json.JSONObject;
-import org.json.JSONArray;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonArray;
+
 
 public class Inventory {
     private static Inventory instance = new Inventory();
@@ -23,23 +26,27 @@ public class Inventory {
     }
 
     public void loadInventory() {
-        try {
-            String content = new String(Files.readAllBytes(Paths.get("inventory.json")));
-            JSONObject json = new JSONObject(content);
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("inventory.json");
+             JsonReader reader = Json.createReader(is)) {
 
-            JSONObject storeInfo = json.getJSONObject("store_info");
+            if (is == null) {
+                throw new FileNotFoundException("Resource not found: inventory.json");
+            }
+
+            JsonObject json = reader.readObject();
+
+            JsonObject storeInfo = json.getJsonObject("store_info");
             storeName = storeInfo.getString("store_name");
             phoneNumber = storeInfo.getString("phone_number");
             city = storeInfo.getString("city");
             state = storeInfo.getString("state");
-            cityTax = storeInfo.getDouble("city_tax");
+            cityTax = storeInfo.getJsonNumber("city_tax").doubleValue();
 
-            JSONArray productArray = json.getJSONArray("product_info");
-            for (int i = 0; i < productArray.length(); i++) {
-                JSONObject prod = productArray.getJSONObject(i);
+            JsonArray productArray = json.getJsonArray("product_info");
+            for (JsonObject prod : productArray.getValuesAs(JsonObject.class)) {
                 String code = prod.getString("product_code");
                 String name = prod.getString("product_name");
-                double price = prod.getDouble("price");
+                double price = prod.getJsonNumber("price").doubleValue();
                 String description = prod.getString("description");
 
                 products.put(code, new Product(code, name, price, description));
